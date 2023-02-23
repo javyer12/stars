64cc5a7e82855d5a257271721c032387471c1e32
javyer <70788090+javyer12@users.noreply.github.com>
2023-02-22 22:25:19 -0600

    fix❌routing

---
+++ src/components/Daily/index.ts
@@ -0,0 +1,18 @@
+
+// type CoverProps = {
+//   url: string
+// }
+
+// export default function Cover({ url }: CoverProps) {
+//   return (
+//     <ContainerBg>
+//       <img
+//         src={url}
+//         alt=""
+//         width="100%"
+//         height="100%"
+//         style={{ objectFit: 'contain' }}
+//       />
+//     </ContainerBg>
+//   )
+// }
